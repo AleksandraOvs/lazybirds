bb6f339f35c594f2f674380dd26d7f98ca6e9cf9
Aleksandraovs <aleksandra.ovsia@gmail.com>
2024-04-07 22:50:07 +0300

    eds

--- wp-content/themes/mm76_ru/woocommerce/single-product/price.php
+++ wp-content/themes/mm76_ru/woocommerce/single-product/price.php
@@ -24,5 +24,5 @@ global $product;
 ?>
 <p class="<?php echo esc_attr( apply_filters( 'woocommerce_product_price_class', 'price' ) ); ?>"><?php echo $product->get_price_html(); ?></p>
 <?php
-	echo do_shortcode('[bfw_cashback_in_product]');
+	//echo do_shortcode('[bfw_cashback_in_product]');
 ?>
